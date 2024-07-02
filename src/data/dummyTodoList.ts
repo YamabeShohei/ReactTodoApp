b4a019bfb9517ca9ae70065b6c377167115c2b07
yamabe.shohei <mfo.obr314314@gmail.com>
2024-07-02 21:27:17 +0900

    リストデータの表示

---
+++ src/data/dummyTodoList.ts
@@ -0,0 +1,19 @@
+import { Todo } from '../types/todo';
+
+export const dummyTodoList: Todo[] = [
+  {
+    id: 1,
+    title: 'task1',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'task2',
+    completed: true,
+  },
+  {
+    id: 3,
+    title: 'task3',
+    completed: false,
+  }
+];
