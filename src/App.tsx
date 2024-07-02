74b0aa93c50161c4e64ba5790a191f8ccbe43dca
YamabeShohei <74035200+YamabeShohei@users.noreply.github.com>
2024-07-02 22:50:35 +0900

    TODOアプリの基本機能の実装 (#1)
    * タイトル作成
    
    * リストデータの表示
    
    * Listコンポーネントを作成
    
    * チェック状態変更処理の実装
    
    * フォーム作成
    
    * 削除ボタンを追加
    
    * 完了TODOを削除するボタンを実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,78 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { TodoList } from './components/TodoList';
+import { AddTodoForm } from './components/AddTodoForm';
+import { TodoSummary } from './components/TodoSummary';
+import { dummyTodoList } from './data/dummyTodoList';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todoList, setTodoList] = useState(dummyTodoList);
+
+  /**
+   * チェック処理
+   * @param id ID番号
+   */
+  const changeCompleted = (id: number) => {
+    setTodoList((prevTodoList) => {
+      return prevTodoList.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+
+        return todo;
+      });
+    });
+  };
+
+  /**
+   * TODOリストのデータ追加
+   * @param title TODOタイトル
+   */
+  const addTodo = (title: string) => {
+    setTodoList((prevTodoList) => {
+      const newTodo = {
+        id: Date.now(),
+        title,
+        completed: false,
+      };
+
+      return [newTodo, ...prevTodoList];
+    });
+  };
+
+  const deleteTodo = (id: number) => {
+    setTodoList(() => {
+      return todoList.filter((todo) => {
+        return todo.id !== id;
+      });
+    });
+  };
+
+  const deleteAllCompleted = () => {
+    setTodoList((prevTodoList) => {
+      return prevTodoList.filter((todo) => {
+        return !todo.completed;
+      });
+    });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="mx-auto mt-10 max-w-xl space-y-10">
+      <h1 className="text-center text-4xl">Todoアプリ</h1>
+      <div className="space-y-5">
+        <AddTodoForm addTodo={addTodo} />
+        <div className="space-y-5 rounded bg-slate-200 p-5">
+          <TodoList
+            todoList={todoList}
+            changeCompleted={changeCompleted}
+            deleteTodo={deleteTodo}
+          />
+          <TodoSummary deleteAllCompleted={deleteAllCompleted} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   );
 }
 
