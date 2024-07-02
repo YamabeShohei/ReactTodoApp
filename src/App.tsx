bba35beb3e701be10f65258ef9b35360c4c5fd4d
yamabe.shohei <mfo.obr314314@gmail.com>
2024-07-02 21:53:55 +0900

    チェック状態変更処理の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import { TodoList } from './components/TodoList';
 import { dummyTodoList } from './data/dummyTodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState(dummyTodoList);
+
+  const changeCompleted = (id: number) => {
+    setTodoList((prevTodoList) => {
+      return prevTodoList.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+
+        return todo;
+      });
+    });
+  };
+
   return (
-    <main className="mx-auto mt-10 max-w-xl">
+    <main className="mx-auto mt-10 max-w-xl space-y-10">
       <h1 className="text-center text-4xl">Todoアプリ</h1>
       <div className="rounded bg-slate-200 p-5">
         <div className="space-y-3">
-          <TodoList todoList={dummyTodoList} />
+          <TodoList todoList={todoList} changeCompleted={changeCompleted} />
         </div>
       </div>
     </main>
